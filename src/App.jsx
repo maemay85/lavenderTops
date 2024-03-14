b54a25d9a2e05747aacdf7d85edcd5b731f205c3
maemay85 <hellomaemay@gmail.com>
2024-03-13 22:37:34 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+
+    <div className='app'>
+      <img src='src/assets/IMG_8511.JPG' />
+      <div className='temp'>
+        <h1>Lavender Tops</h1>
+        <h2>Full site coming soon!</h2>
+        <h3>Check us out on <a href="https://instagram.com/lavendertopsband">Instagram</a></h3>
+        <h3>or reach out to us at: ilovelavendertops@gmail.com</h3>
+      </div>
+    </div>
+  )
+}
+
+export default App
